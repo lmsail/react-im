b46aa7ab36778f6c04b4da07ccbac3d2dfb3fb53
MarichMarck <582639426@qq.com>
2020-03-20 16:25:25 +0800

    ADD README.md

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# react-im
-react + antd + redux 仿微信网页版
+## 简介
+react + antd + redux 仿微信网页版，纯前端项目，未对接后端，数据纯前端模拟
+
+## 技术选型
+
+- react
+- redux
+- antd （3.x）
+
+## 功能
+
+-[x] 会话列表
+-[x] 会话窗口
+-[x] 通讯录
+-[x] 设置
+-[x] 全屏切换
+
+## 预览
+
+**首页**
+![首页](https://github.com/MarichMarck/react-im/raw/master/preview/home.png)
+
+**消息**
+![消息](https://github.com/MarichMarck/react-im/raw/master/preview/message.png)
+
+**通讯录**
+![通讯录](https://github.com/MarichMarck/react-im/raw/master/preview/maillist.png)
+
+**设置**
+![设置](https://github.com/MarichMarck/react-im/raw/master/preview/about.png)
---
+++ build/precache-manifest.bc4b7c4ad90efa95a31d09e0f037f0b7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934d4b4dc8fa743bfd5b8e8f9df70891",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c1f79299d3aaf6ff3c0",
+    "url": "/static/css/2.46b28f8f.chunk.css"
+  },
+  {
+    "revision": "90e41f3245b9c4a17c23",
+    "url": "/static/css/main.c05a920a.chunk.css"
+  },
+  {
+    "revision": "3c1f79299d3aaf6ff3c0",
+    "url": "/static/js/2.ead6fe83.chunk.js"
+  },
+  {
+    "revision": "73b6afecf208e3af16660ced4c4b8325",
+    "url": "/static/js/2.ead6fe83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90e41f3245b9c4a17c23",
+    "url": "/static/js/main.f7390a9b.chunk.js"
+  },
+  {
+    "revision": "29c4995013be4954e500",
+    "url": "/static/js/runtime-main.dcc7b5e5.js"
+  },
+  {
+    "revision": "b46834f8335534e55f928a88e8a17759",
+    "url": "/static/media/redux.b46834f8.png"
+  },
+  {
+    "revision": "5825f033c6ff12cd1ed1f3c99dff5e4b",
+    "url": "/static/media/welcome-bg.5825f033.svg"
+  },
+  {
+    "revision": "bc69af85260c59d7fb9d138321063aa8",
+    "url": "/static/media/welcome.bc69af85.png"
+  }
+]);
